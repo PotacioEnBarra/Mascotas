a439e67042972b0a4a7e510d5ddbfc31376ec81f
Carlos Manchego <carlosm.manchegop@konradlorenz.edu.co>
2020-10-19 17:15:37 -0500

    Primer Commit Carlos
    Se creo la base del proyecto en spring boot.

---
+++ src/main/java/co/edu/konrad/crudmascotas/CrudmascotasApplication.java
@@ -0,0 +1,13 @@
+package co.edu.konrad.crudmascotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudmascotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudmascotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/konrad/crudmascotas/CrudmascotasApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.konrad.crudmascotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudmascotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
